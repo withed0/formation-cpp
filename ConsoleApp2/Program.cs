1951ca79c40f1d90f49776ae3a322b27875be845
Aset CHOVKHALOVA <aset.chovkhalova@viaformation.e-cdp.com>
2023-09-26 13:51:43 +0200

    result

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("donner numero");
+
+string saissie = Console.ReadLine();
+int nombre = int.Parse(saissie);
+int result = nombre * nombre;
+
+Console.WriteLine($"le carré de {nombre} est {result}");
+
