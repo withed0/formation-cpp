feeec0a29cc8bd3f89961599f958bfb104523899
Aset CHOVKHALOVA <aset.chovkhalova@viaformation.e-cdp.com>
2023-10-10 09:39:21 +0200

    exercice fini

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+Console.WriteLine("donner un nombre");
+string numUti = Console.ReadLine();
+int num = int.Parse(numUti);
+if (num>0)
+{
+    Console.WriteLine("le nombre est positif");
+}
+else if (num<0)
+{
+    Console.WriteLine("le nombre est negatif");
+}
+else
+{
+    Console.WriteLine("le nombre est negatif et positif"); 
+}
