ea20baff2ba77d8a712160b74b94138b45eac3f6
Aset CHOVKHALOVA <aset.chovkhalova@viaformation.e-cdp.com>
2023-10-10 11:23:15 +0200

    fini

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("donner moi deux nombre");
+string numUti = Console.ReadLine();
+int num = int.Parse(numUti);
+string numUti2 = Console.ReadLine();
+int num2 = int.Parse(numUti2);
+if (0<num && 0<num2)
+{
+    Console.WriteLine("le produit est positif");
+}
+else if (0>num && 0>num2)
+{
+    Console.WriteLine("le nombre est negatif");
+}
+else
+{
+    Console.WriteLine("le nombre est negatif");
+}
