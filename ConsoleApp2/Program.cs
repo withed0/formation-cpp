ec9b2830b2b78067e42e5a394bd263213e43e683
Aset CHOVKHALOVA <aset.chovkhalova@viaformation.e-cdp.com>
2023-10-10 09:09:45 +0200

    EXERCICE FAUX

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("donner moi deux nombre");
+string numUti = Console.ReadLine();
+int num = int.Parse(numUti);
+string numUti2 = Console.ReadLine();
+int num2 = int.Parse(numUti2);
+if (0<num && num2>0)
+{
+    Console.WriteLine("le produit est posotif")
+}
+else if (0>num && num2<0)
+{
+    Console.WriteLine("le produit est negatif")
+}
+else (0>num && num2>0 || 0<num && num2>0)
+{
+    Console.WriteLine("le produit est negatif")
+}
